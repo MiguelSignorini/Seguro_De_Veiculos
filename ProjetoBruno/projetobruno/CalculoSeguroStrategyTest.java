00772e40a920e46397ecb4e09aa5744a61ab306c
mwlzi <mellfs2004@gmail.com>
2026-06-11 10:51:18 -0300

    test: cria testes unitarios para os cenarios RF040 e RF041 do padrao Strategy

---
+++ ProjetoBruno/projetobruno/CalculoSeguroStrategyTest.java
@@ -0,0 +1,39 @@
+package projetobruno;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import projetobruno.strategy.CalculadoraSeguroContexto;
+import projetobruno.strategy.EstrategiaFrotaComercial;
+import projetobruno.strategy.EstrategiaCondutorJovem;
+
+public class CalculoSeguroStrategyTest {
+
+    @Test
+    public void testRF040_ExecutarCalculoEmpresarialFrotaComDesconto() {
+        CalculadoraSeguroContexto contexto = new CalculadoraSeguroContexto();
+        double valorBasePorVeiculo = 1000.0;
+        int quantidadeFrota = 5;
+        int idadeCondutor = 40;
+
+        contexto.setEstrategia(new EstrategiaFrotaComercial());
+        double resultadoObtido = contexto.executarCalculo(valorBasePorVeiculo, quantidadeFrota, idadeCondutor);
+
+        double resultadoEsperado = 4500.0;
+        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
+    }
+
+    @Test
+    public void testRF041_ExecutarCalculoCondutorJovemComRiscoAgravado() {
+        CalculadoraSeguroContexto contexto = new CalculadoraSeguroContexto();
+        double valorBaseVeiculo = 1000.0;
+        int quantidadeVeiculos = 1;
+        int idadeCondutorJovem = 22;
+
+        contexto.setEstrategia(new EstrategiaCondutorJovem());
+        double resultadoObtido = contexto.executarCalculo(valorBaseVeiculo, quantidadeVeiculos, idadeCondutorJovem);
+
+        double resultadoEsperado = 1300.0;
+        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
+    }
+}
